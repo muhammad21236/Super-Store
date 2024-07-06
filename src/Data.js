36a860f46310b2b3a669715ad371d333dd859179
Muhammad Atta <155086759+muhammad21236@users.noreply.github.com>
2024-07-06 15:31:36 +0500

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Kinder CHOCOLATE 8 BARS",
+    price: 10,
+    desc: "Kinder Chocolate is a delicious, creamy milk chocolate bar that is filled with a smooth melting milk cream. ",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Kinder Joy Eggs",
+    price: 25,
+    desc: "Kinder Joy Eggs, Bulk 15 Count, Treat Plus Toy, Sweet Cream and Chocolatey Wafers, Individually Wrapped, 10.5 oz, Packaging May Vary",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Kinder Bueno ice creams",
+    price: 30,
+    desc: "Ice cream and hazelnut chocolate syrup (5% of which 10% milk chocolate), a cone biscuit (13%) cocoa coating (6%), and the top plate with hazelnut paste (12%) and chocolate (1.5 %).",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "LAY'S® BBQ Flavored Potato Chips",
+    price: 12,
+    desc: "It all starts with farm-grown potatoes, cooked and seasoned to perfection. Then we add the spicy sweetness of BBQ sauce. So every LAY'S® potato chip is perfectly crispy and delicious. Happiness in Every Bite.®",
+    img: "images/img-4.jpg",
+  },
+];
